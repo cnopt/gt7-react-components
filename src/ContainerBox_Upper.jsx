0bc6d9e8787c987a9c529d63e34c5cd4805971a8
cnopt <cbiddisc@gmail.com>
2023-02-08 16:44:55 +0000

    containerbox upper padding

--- src/ContainerBox_Upper.jsx
+++ src/ContainerBox_Upper.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './css/ContainerBox_Upper.css'
 
 export default function ContainerBox_Upper(props) {
     const {children} = props;
     return(
         <>
-            <div class="containerbox-upper">
+            <div id="containerbox-upper">
                 {children}
             </div>
         </>
